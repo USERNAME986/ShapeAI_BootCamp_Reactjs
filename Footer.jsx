4f1afed98f2694b5128c7dba0f6736b9183b0c8e
Username 986 <86418985+USERNAME986@users.noreply.github.com>
2021-06-26 20:51:45 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright by USERNAME @ {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>USERNAME'S website</h1>
+    </header>
+  );
+}
+
+export default Header;
